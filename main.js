95ba3276cfc36dde54c1ff21272687db1e997249
cloudnote2027 <cloud@nomor.shop>
2026-04-13 01:11:57 +0630

    Update main.js

--- main.js
+++ main.js
@@ -5,19 +5,19 @@ module.exports = async function (context) {
     const client = new sdk.Client();
     const databases = new sdk.Databases(client);
 
-    // SGP Endpoint ကို တိုက်ရိုက်သုံးထားပါတယ်
     client
-        .setEndpoint('https://sgp.cloud.appwrite.io/v1') 
-        .setProject('69dbc2e7002c8efa0c80') 
-        .setKey('standard_2ce8b8e77513955e33e50d655f0eb1da84ac7cc2f1c1d14d571a5f70f1e850aea9c433448474ad9a16e6068c1be8e57acddd679f8588f79f06424dc0cfa6156f032a148736870b06da0afd791508f5f3de0e6a17777cf8f10bd70e2c50dee1d14804d7b119d11235179bd3fd7bfc490d356da4138ecdc1ad8d07f6edfb1e4d19');
+        .setEndpoint('https://cloud.appwrite.io/v1') 
+        .setProject('69dbc2e7002c8efa0c80') // ကိုမျိုးရဲ့ Project ID
+        .setKey('standard_2ce8b8e77513955e33e50d655f0eb1da84ac7cc2f1c1d14d571a5f70f1e850aea9c433448474ad9a16e6068c1be8e57acddd679f8588f79f06424dc0cfa6156f032a148736870b06da0afd791508f5f3de0e6a17777cf8f10bd70e2c50dee1d14804d7b119d11235179bd3fd7bfc490d356da4138ecdc1ad8d07f6edfb1e4d19'); // ကိုမျိုးရဲ့ API Key
 
     try {
         const response = await axios.get('https://api.wingogame.com/get-latest-results'); 
         const data = response.data;
 
+        // ဒီမှာ Database ID နဲ့ Collection ID ကိုပဲ သုံးရပါတယ်
         await databases.createDocument(
-            '69dbcab6001e18fba9ec', 
-            'game_data_logs', 
+            '69dbcab6001e18fba9ec', // Database ID
+            'game_data_logs',        // Collection ID
             sdk.ID.unique(),
             {
                 issueNumber: data.period,
